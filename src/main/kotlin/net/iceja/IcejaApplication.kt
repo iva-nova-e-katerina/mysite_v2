edd2732fefe39ae0f1211a91a03e578dc7910069
Ekaterina Ivanova <ekaterina.moskva2016@yandex.ru>
2023-05-19 09:38:33 +0300

    adding internationalization

--- src/main/kotlin/net/iceja/IcejaApplication.kt
+++ src/main/kotlin/net/iceja/IcejaApplication.kt
@@ -8,10 +8,38 @@ package net.iceja
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.web.servlet.LocaleResolver
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.web.servlet.i18n.CookieLocaleResolver
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor
+import org.springframework.web.servlet.i18n.SessionLocaleResolver
+import java.util.*
+
 
 @SpringBootApplication
 @EnableConfigurationProperties(IcejaProperties::class)
-class IcejaApplication
+class IcejaApplication : WebMvcConfigurer {
+    @Bean
+    fun localeResolver(): LocaleResolver? {
+        val slr = CookieLocaleResolver()
+        slr.setDefaultLocale(Locale.US)
+        return slr
+    }
+
+    @Bean
+    fun localeChangeInterceptor(): LocaleChangeInterceptor? {
+        val lci = LocaleChangeInterceptor()
+        lci.paramName = "lang"
+        return lci
+    }
+
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(localeChangeInterceptor()!!)
+    }
+
+}
 
 fun main(args: Array<String>) {
     runApplication<IcejaApplication>(*args)
